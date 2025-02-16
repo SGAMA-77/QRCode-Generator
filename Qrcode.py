869655f73c134e664a043f793571fabaf58f0056
SGAMA-77 <kessie.sigama@gmail.com>
2025-02-16 22:05:53 +0200

    Added the code

--- Qrcode.py
+++ Qrcode.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import qrcode
+
+linkedIn_profile = "https://www.linkedin.com/in/phophi-sigama-7ba192236"
+
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+
+qr.add_data(linkedIn_profile)
+qr.make()
+
+qr_img = qr.make_image(fill_color = 'black', back_color = 'white')
+
+qr_img.save('linkedIn Profile.png')
